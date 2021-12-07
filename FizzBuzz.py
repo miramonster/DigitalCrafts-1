ea6208296b93bef8f69f8b34e695054e799370b5
Jason <jasonpaff@gmail.com>
2021-12-07 15:52:14 -0500

    fixed

--- FizzBuzz.py
+++
@@ -1,8 +0,0 @@
-number = int(input("Enter a whole number: "))
-
-if number % 3 == 0 and number % 5 == 0:
-    print ("FizzBuzz")
-elif number % 3 == 0:
-    print ("Fizz")
-elif number % 5 == 0:
-    print ("Buzz")
