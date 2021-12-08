6857268966786e2e3a71d5b95b13651e9f6e7fe3
Jason <JasonPaff@gmail.com>
2021-12-08 15:32:22 -0500

    12/8/21 Homework

---
+++ PrimeOrNot.py
@@ -0,0 +1,18 @@
+#checks if the given number is a prime number
+def is_prime(number):    
+    #numbers one or less cannot be a prime number
+    if number <= 1:
+        return False
+    #check for factors, if a factor is found then the number is not a prime number
+    for index in range(2, number):
+        if number % index == 0:
+            return False    
+    # anything with no factors is a prime number
+    return True
+
+user_number = int(input("Enter a number: "))
+
+if (is_prime(user_number)):
+    print("Prime Number")
+else:
+    print("Not a Prime Number")
