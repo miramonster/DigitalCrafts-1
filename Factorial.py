5114380977640fc91c97a0547794f12ae0df3fe5
Jason <JasonPaff@gmail.com>
2021-12-08 15:32:53 -0500

    12/8/21 Homework

---
+++ Factorial.py
@@ -0,0 +1,9 @@
+# returns the factorial of a given number
+def factorial (number):
+    for index in range(2, number):
+        number *= index
+    return number
+
+user_number = int(input("Enter a whole number: "))
+
+print(factorial(user_number))
