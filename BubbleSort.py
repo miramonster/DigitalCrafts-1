abd0cc5fa5204584179e8830505653585ba00e53
Jason <JasonPaff@gmail.com>
2021-12-10 18:31:37 -0500

    12/10/21 Classwork

--- BubbleSort.py
+++ BubbleSort.py
@@ -2,9 +2,11 @@ numbers = [2,1,45,67,89,4,5,7,9]
 
 print(numbers)
 
-for first in range(0,len(numbers)):
+for first in range(0,len(numbers) - 1):
     for index in range(0, len(numbers) - first - 1):
         if (numbers[index] > numbers[index + 1]):
-            numbers[index], numbers[index+1] = numbers[index+1], numbers[index]
-
+            temp = numbers[index]
+            numbers[index] = numbers[index + 1]
+            numbers[index + 1] = temp
+            
 print(numbers)
