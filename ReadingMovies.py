fe1cc9c049e3008578adf4f3e786a2c6f23e4df7
Jason <JasonPaff@gmail.com>
2021-12-16 11:40:19 -0500

    12/16/21 Activity 3

---
+++ ReadingMovies.py
@@ -0,0 +1,11 @@
+import json
+
+movies = ""
+
+with open("movies.json") as file:
+    movies = json.load(file)
+
+for movie in movies['Search']:
+    print(f"\nTitle: {movie['Title']} - Year: {movie['Year']} - Type: {movie['Type']} \nimdbID: {movie['imdbID']} - Poster: {movie['Poster']}")    
+
+print()
