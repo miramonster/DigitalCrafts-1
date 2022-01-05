cd31a0d8028f4194cbc7dd84d4d5eacebd134fff
Jason <JasonPaff@gmail.com>
2022-01-05 18:56:29 -0500

    1/5/22 Dishes Homework

--- Dishes/app.js
+++ Dishes/app.js
@@ -5,7 +5,7 @@ const entreeDishesButton = document.getElementById("entreeDishesButton");
 const dessertDishesButton = document.getElementById("dessertDishesButton");
 
 function MapDishesToListItems(dishes) {
-    return dishes.map(function(dish) {
+    dishesUL.innerHTML = dishes.map(function(dish) {
       return `<li>
         <h3>${dish.title}</h3>
         <h4>${dish.price}</h4>
@@ -16,19 +16,19 @@ function MapDishesToListItems(dishes) {
 };
 
 allDishesButton.addEventListener("click", function () {
-    dishesUL.innerHTML = MapDishesToListItems(dishes);
+    MapDishesToListItems(dishes);
 });
 
 starterDishesButton.addEventListener("click", function() { 
-    dishesUL.innerHTML = MapDishesToListItems(dishes.filter(dish => dish.course == "Starters"))
+    MapDishesToListItems(dishes.filter(dish => dish.course == "Starters"))
 });
 
 entreeDishesButton.addEventListener("click", function () {
-    dishesUL.innerHTML = MapDishesToListItems(dishes.filter(dish => dish.course == "Entrees"))
+    MapDishesToListItems(dishes.filter(dish => dish.course == "Entrees"))
 });
 
 dessertDishesButton.addEventListener("click", function () {
-    dishesUL.innerHTML = MapDishesToListItems(dishes.filter(dish => dish.course == "Desserts"))
+    MapDishesToListItems(dishes.filter(dish => dish.course == "Desserts"))
 });
 
 MapDishesToListItems(dishes);
