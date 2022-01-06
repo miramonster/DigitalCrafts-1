040da13958bf13bd33e69e6203342573cae26f02
Jason Paff <jasonpaff@gmail.com>
2022-01-06 13:14:48 -0500

    1/6/22 Activity Stock Quotes

---
+++ Stock Quotes/app.js
@@ -0,0 +1,23 @@
+const showQuotesButton = document.getElementById("showQuotesButton");
+const showQuotesDiv = document.getElementById("showQuotesDiv");
+const symbolTextBox = document.getElementById("symbolTextBox");
+
+showQuotesButton.addEventListener("click", function() {
+    // store the users stock symbol
+    let symbol = symbolTextBox.value;
+
+    // get stock info using symbol
+    stock = getStockQuote(symbol);
+    
+    // set initial stock display info
+    showQuotesDiv.innerHTML = `<h2>${stock.name}</h2> <h3>${stock.price}</h3>`
+
+    // start 2 second timer to update price
+    window.setInterval(function () {
+        // update stock price
+        stock = getStockQuote(symbol);  
+
+        // update stock display info
+        showQuotesDiv.innerHTML = `<h2>${stock.name}</h2> <h3>${stock.price}</h3>`
+    }, 2000)
+})
