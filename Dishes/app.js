4fe349887ade3cb19d19f66282dd9cd1b5222710
Jason Paff <jasonpaff@gmail.com>
2022-01-06 10:50:27 -0500

    1/5/22 Dishes Homework

--- Dishes/app.js
+++ Dishes/app.js
@@ -1,4 +1,5 @@
 const dishesUnorderedList = document.getElementById("dishes_ul");
+const dishesDropDownList = document.getElementById("courseSelect")
 const allDishesButton = document.getElementById("allDishesButton");
 const starterDishesButton = document.getElementById("starterDishesButton");
 const entreeDishesButton = document.getElementById("entreeDishesButton");
@@ -8,14 +9,18 @@ function MapDishesToListItems(dishes) {
   dishesUnorderedList.innerHTML = dishes.map(dish => 
     `<li>
       <img src=${dish.imageURL} class='dish_image'></img>
-      <h3>${dish.title} - $${dish.price}</h4>
+      <h3><b>${dish.title} - $${dish.price}</b</h4>
       <h5 class='dish_description'>${dish.description}</h5>
     </li>`).join('')
 };
 
-allDishesButton.addEventListener("click", () => MapDishesToListItems(dishes));
-starterDishesButton.addEventListener("click", () => MapDishesToListItems(dishes.filter(dish => dish.course == "Starters")));
-entreeDishesButton.addEventListener("click", () => MapDishesToListItems(dishes.filter(dish => dish.course == "Entrees")));
-dessertDishesButton.addEventListener("click", () => MapDishesToListItems(dishes.filter(dish => dish.course == "Desserts")));
+function FilterDishesDropDown() {
+  const selectedCourse = dishesDropDownList.value;
+
+  if (selectedCourse == "All Dishes") MapDishesToListItems(dishes);
+  else MapDishesToListItems(dishes.filter(dish => dish.course == selectedCourse));
+}
+
+dishesDropDownList.addEventListener("change", () => FilterDishesDropDown());
 
 MapDishesToListItems(dishes);
