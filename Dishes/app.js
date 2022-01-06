17e41846b3b7f92efe9138ffd90097a206bf6f31
Jason <JasonPaff@gmail.com>
2022-01-05 19:29:03 -0500

    1/5/22 Dishes Homework

--- Dishes/app.js
+++ Dishes/app.js
@@ -1,13 +1,9 @@
-// unordered list for holding dishes list items
 const dishesUL = document.getElementById("dishesList");
-
-// four buttons for filtering dishes
 const allDishesButton = document.getElementById("allDishesButton");
 const starterDishesButton = document.getElementById("starterDishesButton");
 const entreeDishesButton = document.getElementById("entreeDishesButton");
 const dessertDishesButton = document.getElementById("dessertDishesButton");
 
-// takes in an array of dishes and maps it to dishes list items for the dishes unordered list
 function MapDishesToListItems(dishes) {
     dishesUL.innerHTML = dishes.map(function(dish) {
       return `<li>
@@ -18,14 +14,9 @@ function MapDishesToListItems(dishes) {
   }).join('')
 };
 
-// add click events to the four buttons to filter the dishes to the desired course
 allDishesButton.addEventListener("click", () => MapDishesToListItems(dishes));
-
 starterDishesButton.addEventListener("click", () => MapDishesToListItems(dishes.filter(dish => dish.course == "Starters")));
-
 entreeDishesButton.addEventListener("click", () => MapDishesToListItems(dishes.filter(dish => dish.course == "Entrees")));
-
 dessertDishesButton.addEventListener("click", () => MapDishesToListItems(dishes.filter(dish => dish.course == "Desserts")));
 
-// show all the dishes initially
 MapDishesToListItems(dishes);
