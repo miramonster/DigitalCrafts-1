779fa79ee5a12f4cc12e4ccad7994f221e288e1a
Jason Paff <jasonpaff@gmail.com>
2022-01-05 23:20:47 -0500

    1/5/22 Dishes Homework

--- Dishes/app.js
+++ Dishes/app.js
@@ -1,28 +1,21 @@
-// dishes unordered list
 const dishesUnorderedList = document.getElementById("dishes_ul");
-
-// four buttons for dish filtering
 const allDishesButton = document.getElementById("allDishesButton");
 const starterDishesButton = document.getElementById("starterDishesButton");
 const entreeDishesButton = document.getElementById("entreeDishesButton");
 const dessertDishesButton = document.getElementById("dessertDishesButton");
 
-// maps a passed in array of dishes to list items for the dishes unordered list
 function MapDishesToListItems(dishes) {
-    dishesUnorderedList.innerHTML = dishes.map(function(dish) {
-      return `<li>
-            <img src=${dish.imageURL} class='dish_image'></img>
-            <h3>${dish.title} - $${dish.price}</h4>
-            <h5 class='dish_description'>${dish.description}</h5>
-        </li>`
-  }).join('') 
+  dishesUnorderedList.innerHTML = dishes.map(dish => 
+    `<li>
+      <img src=${dish.imageURL} class='dish_image'></img>
+      <h3>${dish.title} - $${dish.price}</h4>
+      <h5 class='dish_description'>${dish.description}</h5>
+    </li>`).join('') 
 };
 
-// add click events to the four buttons to filter to specific courses
 allDishesButton.addEventListener("click", () => MapDishesToListItems(dishes));
 starterDishesButton.addEventListener("click", () => MapDishesToListItems(dishes.filter(dish => dish.course == "Starters")));
 entreeDishesButton.addEventListener("click", () => MapDishesToListItems(dishes.filter(dish => dish.course == "Entrees")));
 dessertDishesButton.addEventListener("click", () => MapDishesToListItems(dishes.filter(dish => dish.course == "Desserts")));
 
-// show all courses when page is first loaded
 MapDishesToListItems(dishes);
