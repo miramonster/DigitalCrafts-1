54b0c2624ea04a4c92c06fca04381122528f6f39
Jason Paff <jasonpaff@gmail.com>
2022-01-07 12:08:32 -0500

    1/7/22 Activity Registration Screen

---
+++ Registration Screen/app.js
@@ -0,0 +1,7 @@
+const submitButton = document.getElementById("submitButton")
+const inputForm = document.getElementById("inputForm")
+
+submitButton.addEventListener("click", function() {
+    let isFormValid = inputForm.checkValidity()
+    }
+})
