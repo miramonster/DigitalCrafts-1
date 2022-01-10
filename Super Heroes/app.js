1d1efcd5e40c352326f51cf8e54317ed73eb8e30
Jason Paff <jasonpaff@gmail.com>
2022-01-10 15:53:17 -0500

    1/10/22 Homework Super Heroes

---
+++ Super Heroes/app.js
@@ -0,0 +1,56 @@
+const superHeroDiv = document.getElementById("superHeroDiv")
+const searchButton = document.getElementById("searchButton")
+const searchTextBox = document.getElementById("searchTextBox")
+const apikey = '252c9978'
+
+searchButton.onclick = () => GetSearchResults()
+
+function GetSearchResults(){
+    let superHero = searchTextBox.value
+    let request = new XMLHttpRequest()
+
+    request.open('GET', `http://www.omdbapi.com/?s=${superHero}&apikey=${apikey}`)
+    request.send()
+    
+    request.onload = function() {
+        let result = JSON.parse(this.responseText)
+        DisplaySearchResults(result.Search)
+    }
+}
+
+function DisplaySearchResults(results){
+    let images = results.map(result => 
+        `<div class="search">
+        <img src=${result.Poster} class="poster"></img>
+        <h5>${result.Title} (${result.Year})</h5>
+        <button id="moreInfoButton" onclick="GetImdbInfo('${result.imdbID}')">More Info</button>
+        <div id="${result.Title}InfoDiv"></div>
+        </div>`).join('')
+
+    superHeroDiv.innerHTML = images
+}
+
+function DisplayImdbInfo(result){    
+    let results = []
+    results.push(result)
+    let info = results.map(result =>
+        `<h5>Metascore: ${result.Metascore}</h5>
+        <h5>imdb Rating: ${result.imdbRating}</h5>
+        <h5>MPAA Rating: ${result.Rated}</h5>
+        <h5>Runtime: ${result.Runtime}</h5>
+        `).join('')
+ 
+    let div = document.getElementById(`${result.Title}InfoDiv`) 
+    div.innerHTML = info    
+}
+
+function GetImdbInfo(id){    
+    let request = new XMLHttpRequest()
+    request.onload = function() {
+        let result = JSON.parse(this.responseText)
+        DisplayImdbInfo(result)
+    }
+
+    request.open('GET', `http://www.omdbapi.com/?i=${id}&apikey=${apikey}`)
+    request.send()
+}
