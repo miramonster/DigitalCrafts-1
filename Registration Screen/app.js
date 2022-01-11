61b31dabd8a45d5837e1b0b8213d7685696061d5
Jason Paff <jasonpaff@gmail.com>
2022-01-11 13:02:40 -0500

    update

--- Registration Screen/app.js
+++ Registration Screen/app.js
@@ -2,6 +2,5 @@ const submitButton = document.getElementById("submitButton")
 const inputForm = document.getElementById("inputForm")
 
 submitButton.addEventListener("click", function() {
-    let isFormValid = inputForm.checkValidity()
-    }
+    let isFormValid = inputForm.checkValidity()    
 })
