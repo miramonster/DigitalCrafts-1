3f37e344e1ad35803d6804b9329d571d1e743ad2
Jason Paff <jasonpaff@gmail.com>
2022-01-12 13:29:59 -0500

    1/12/22 Homework WeatherApp

--- Weather App/app.js
+++ Weather App/app.js
@@ -1,25 +1,27 @@
-let checkWeatherButton = document.getElementById("checkWeatherButton")
-let cityTextBox = document.getElementById("cityTextBox")
+const apiByCityUrl = 'http://api.openweathermap.org/data/2.5/weather?q='
+const apiByLongLatUrl = 'http://api.openweathermap.org/data/2.5/weather?lat='
+const apiKey = '4bc94f2afc12e496789ee0829f4ca267'
 
-let apiByCityUrl = 'http://api.openweathermap.org/data/2.5/weather?q='
-let apiByLongLatUrl = 'http://api.openweathermap.org/data/2.5/weather?lat='
-let apiKey = '4bc94f2afc12e496789ee0829f4ca267'
+document.getElementById("checkWeatherButton").onclick = () => getWeatherDataFromCityName()
 
-checkWeatherButton.onclick = () => getWeatherDataFromCityName()
+// display the users weather when page loads
+getUserLongLat();
 
-getUserLongLat()
-
+// return the weather data using a city name
 function getWeatherDataFromCityName(){
+    let cityTextBox = document.getElementById("cityTextBox")
     fetch(`${apiByCityUrl}${cityTextBox.value}&appid=${apiKey}&units=imperial`)
     .then(response => response.json())
     .then(result => displayWeatherData(result))
 }
 
+// display the weather data
 function displayWeatherData(weatherData){
     let weatherDataDiv = document.getElementById("weatherDataDiv")
     weatherDataDiv.innerHTML = formatWeatherData(weatherData)
 }
 
+// format the weather data for display
 function formatWeatherData(weatherData){
     return `
     <ul style='list-style-type: none'>
@@ -31,19 +33,16 @@ function formatWeatherData(weatherData){
     </ul>`
 }
 
+// return the long/lat of the user
 function getUserLongLat(){
     if ('geolocation' in navigator)
         navigator.geolocation.getCurrentPosition(position => 
             getWeatherDataFromLongLat(position.coords.longitude, position.coords.latitude))
 }
 
+// return the weather data from a longitdue and latitude
 function getWeatherDataFromLongLat(longitude, latitude) {  
     fetch(`${apiByLongLatUrl}${latitude}&lon=${longitude}&appid=${apiKey}&units=imperial`)
     .then(response => response.json())
-    .then(result => displayUserWeatherData(result))
-}
-
-function displayUserWeatherData(weatherData){
-    let userWeatherDataDiv = document.getElementById("userWeatherDataDiv")
-    userWeatherDataDiv.innerHTML = formatWeatherData(weatherData)
+    .then(result => displayWeatherData(result))
 }
