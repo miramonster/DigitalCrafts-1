ec15ab1b205b6634df1354a0bc6d69dc97f68df3
Jason Paff <jasonpaff@gmail.com>
2022-01-12 16:15:41 -0500

    1/12/22 Homework Weather App

--- Weather App/app.js
+++ Weather App/app.js
@@ -2,15 +2,15 @@ const apiByCityUrl = 'http://api.openweathermap.org/data/2.5/weather?'
 const apiByLongLatUrl = 'http://api.openweathermap.org/data/2.5/weather?'
 const apiKey = '4bc94f2afc12e496789ee0829f4ca267'
 
-document.getElementById("checkWeatherButton").onclick = () => getWeatherDataFromCityName()
+let cityTextBox = document.getElementById("cityTextBox")
+document.getElementById("checkWeatherButton").onclick = () => getWeatherDataFromCityName(cityTextBox.value)
 
 // start the process of displaying weather based on the users location when the page loads
 getUserLongLat();
 
 // return the weather data using a city name
-function getWeatherDataFromCityName(){
-    let cityTextBox = document.getElementById("cityTextBox")
-    fetch(`${apiByCityUrl}q=${cityTextBox.value}&appid=${apiKey}&units=imperial`)
+function getWeatherDataFromCityName(cityName){
+    fetch(`${apiByCityUrl}q=${cityName}&appid=${apiKey}&units=imperial`)
     .then(response => response.json())
     .then(result => displayWeatherData(result))
 }
