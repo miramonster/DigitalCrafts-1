fae6ac5519a65b0732927b965ac67af44c39ae11
Jason Paff <jasonpaff@gmail.com>
2022-01-12 13:09:39 -0500

    1/12/22 Homework WeatherApp

--- Weather App/app.js
+++ Weather App/app.js
@@ -1,65 +1,49 @@
 let checkWeatherButton = document.getElementById("checkWeatherButton")
 let cityTextBox = document.getElementById("cityTextBox")
 
-let apiUrl = 'http://api.openweathermap.org/data/2.5/weather?q='
+let apiByCityUrl = 'http://api.openweathermap.org/data/2.5/weather?q='
+let apiByLongLatUrl = 'http://api.openweathermap.org/data/2.5/weather?lat='
 let apiKey = '4bc94f2afc12e496789ee0829f4ca267'
 
-checkWeatherButton.onclick = () => getWeatherData()
+checkWeatherButton.onclick = () => getWeatherDataFromCityName()
 
-getUserLocation()
+getUserLongLat()
 
-function getWeatherData(){
-    fetch(`${apiUrl}${cityTextBox.value}&appid=${apiKey}&units=imperial`)
+function getWeatherDataFromCityName(){
+    fetch(`${apiByCityUrl}${cityTextBox.value}&appid=${apiKey}&units=imperial`)
     .then(response => response.json())
     .then(result => displayWeatherData(result))
 }
 
 function displayWeatherData(weatherData){
     let weatherDataDiv = document.getElementById("weatherDataDiv")
-    let currentTemp = weatherData.main.temp
-    let minTemp = weatherData.main.temp_min
-    let maxTemp = weatherData.main.temp_max
-    let cityName = weatherData.name
-    let pressure = weatherData.main.pressure
-
-    weatherDataDiv.innerHTML = 
-        `<ul style='list-style-type: none'>
-            <li>City: ${cityName}</li>
-            <li>Current Temp: ${currentTemp} degrees</li>
-            <li>Max Temp: ${maxTemp} degrees</li>
-            <li>Min Temp: ${minTemp} degrees</li>
-            <li>Pressure: ${pressure} atmospheres</li>
-        </ul>`
+    weatherDataDiv.innerHTML = formatWeatherData(weatherData)
 }
 
-function getUserLocation(){
+function formatWeatherData(weatherData){
+    return `
+    <ul style='list-style-type: none'>
+        <li>City: ${weatherData.name}</li>
+        <li>Current Temp: ${weatherData.main.temp} degrees</li>
+        <li>Max Temp: ${weatherData.main.temp_max} degrees</li>
+        <li>Min Temp: ${weatherData.main.temp_min} degrees</li>
+        <li>Pressure: ${weatherData.main.pressure} atmospheres</li>
+    </ul>`
+}
+
+function getUserLongLat(){
     if ('geolocation' in navigator)
         navigator.geolocation.getCurrentPosition(position => 
-            getUserWeatherData(position.coords.longitude, position.coords.latitude))
+            getWeatherDataFromLongLat(position.coords.longitude, position.coords.latitude))
 }
 
-function getUserWeatherData(longitude, latitude) {  
-    let apiUrl = 'http://api.openweathermap.org/data/2.5/weather?lat='
-    fetch(`${apiUrl}${latitude}&lon=${longitude}&appid=${apiKey}&units=imperial`)
+function getWeatherDataFromLongLat(longitude, latitude) {  
+    fetch(`${apiByLongLatUrl}${latitude}&lon=${longitude}&appid=${apiKey}&units=imperial`)
     .then(response => response.json())
     .then(result => displayUserWeatherData(result))
 }
 
 function displayUserWeatherData(weatherData){
     let userWeatherDataDiv = document.getElementById("userWeatherDataDiv")
-    let currentTemp = weatherData.main.temp
-    let minTemp = weatherData.main.temp_min
-    let maxTemp = weatherData.main.temp_max
-    let cityName = weatherData.name
-    let pressure = weatherData.main.pressure
-
-    userWeatherDataDiv.innerHTML = 
-        `<ul style='list-style-type: none'>
-            Your Local Weather!
-            <li>City: ${cityName}</li>
-            <li>Current Temp: ${currentTemp} degrees</li>
-            <li>Max Temp: ${maxTemp} degrees</li>
-            <li>Min Temp: ${minTemp} degrees</li>
-            <li>Pressure: ${pressure} atmospheres</li>
-        </ul>`
+    userWeatherDataDiv.innerHTML = formatWeatherData(weatherData)
 }
